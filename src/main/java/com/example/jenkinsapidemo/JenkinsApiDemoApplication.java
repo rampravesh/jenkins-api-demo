daee8fb4705d2e9c6632b5d7b69fe047e442eceb
ram.chauhan <ram.chauhan@tavant.com>
2020-05-31 12:48:10 +0530

    First commit

---
+++ src/main/java/com/example/jenkinsapidemo/JenkinsApiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsApiDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jenkinsapidemo/JenkinsApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsapidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
